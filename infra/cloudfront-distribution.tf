ed2cf54569b88a9fe77f7ab74120bf64a758adb4
Nisanth Chunduru <chunisan@amazon.com>
2026-03-02 00:14:10 +0530

    Modify Cloudfront distribution so the blog doesn't display "Access Denied" when a user visits any url (except home page) directly

--- infra/cloudfront-distribution.tf
+++ infra/cloudfront-distribution.tf
@@ -136,6 +136,12 @@ resource "aws_cloudfront_distribution" "blog_frontend" {
   }
 
   custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
     error_code         = 404
     response_code      = 200
     response_page_path = "/index.html"
