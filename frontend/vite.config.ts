d17f72e966fc762bb9ef683432d143e6d33d23c2
Nisanth Chunduru <nisanth074@gmail.com>
2026-03-16 08:44:20 +0530

    Configure Vite to create a separate JS file containing vendor JS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'react-helmet-async'],
+        },
+      },
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 3000,
