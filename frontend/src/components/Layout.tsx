52898ac428153d267218dea9b906c9dfbad0e52d
Nisanth Chunduru <chunisan@amazon.com>
2026-03-16 17:56:00 +0530

    Load Google fonts after app is mounted

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
+import GoogleFonts from './GoogleFonts'
 import Header from './Header'
 import Footer from './Footer'
 import TopLoadingBar from './TopLoadingBar'
@@ -20,10 +21,8 @@ export default function Layout() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="google-site-verification" content="CIGnBZCqH2WX_UMtk5WQP3UJg6UIk_2HNNlBAnLFatA" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,300..700;1,300..700&display=swap" />
       </Helmet>
+      <GoogleFonts />
       <div className="flex flex-col dark:bg-brand-dark" style={{ minHeight: '100vh' }}>
         <TopLoadingBar />
         <Header />
